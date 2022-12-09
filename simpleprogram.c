7a27ba676f1b96213fbfc1029759b1ae25c1368e
Debian <azureuser@debian.14vd4ostgc2etjqt0rnjzqnlra.ax.internal.cloudapp.net>
2022-12-09 17:35:38 +0000

    adding welcome message and prompt for user input

---
+++ simpleprogram.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//simple program to print out a message to the console when run
+
+int main()
+{
+ int yourName;
+
+ printf("Hello, Nice to meet you. What's your name?);
+
+//prompt for user to enter name:
+
+scanf("%d",&yourName); 
+}
